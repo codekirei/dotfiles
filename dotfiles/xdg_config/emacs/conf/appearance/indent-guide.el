b94dbeeeadab32661a352f94fed75ddc3f59d1d2
Jacob Blakely <jacob@codekirei.com>
2015-09-25 15:48:07 -0700

    EMACS: add indent-guide

---
+++ dotfiles/xdg_config/emacs/conf/appearance/indent-guide.el
@@ -0,0 +1,7 @@
+(use-package indent-guide
+  :config
+  (indent-guide-global-mode)
+  (setq indent-guide-recursive t)
+  (set-face-foreground 'indent-guide-face "color-235")
+  (setq indent-guide-char "┃")
+)
