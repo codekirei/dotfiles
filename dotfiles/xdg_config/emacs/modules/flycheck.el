8d873af76b3d963f03653ce1e058c8d50dd6a50f
Jacob Blakely <jacob@codekirei.com>
2015-09-27 18:24:43 -0700

    EMACS: flycheck -> local instead of global

--- dotfiles/xdg_config/emacs/modules/flycheck.el
+++ dotfiles/xdg_config/emacs/modules/flycheck.el
@@ -1,6 +1,7 @@
 (use-package flycheck
   :config
-  (add-hook 'after-init-hook 'global-flycheck-mode)
+  ;; (add-hook 'after-init-hook 'global-flycheck-mode)
+  (add-hook 'js-mode-hook 'flycheck-mode)
   (diminish 'flycheck-mode)
   (setq flycheck-highlighting-mode nil)
   (setq flycheck-indication-mode nil)
