37d2c816a20fe404cf3718452cf96dfa622a13e7
Jacob Blakely <jacob@codekirei.com>
2026-03-27 16:39:43 -0700

    oops fix formatting

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,51 +1,51 @@
 local parsers = {
-	"bash",
-	"c",
-	"css",
-	"csv",
-	"diff",
-	"dockerfile",
-	"elixir",
-	"erlang",
-	"go",
-	"gomod",
-	"gpg",
-	"graphql",
-	"html",
-	"javascript",
-	"jsdoc",
-	"json",
-	"just",
-	"lua",
-	"make",
-	"markdown",
-	"markdown_inline",
-	"python",
-	"query",
-	"rust",
-	"scss",
-	"sql",
-	"terraform",
-	"typescript",
-	"vim",
-	"vimdoc",
-	"yaml",
+  'bash',
+  'c',
+  'css',
+  'csv',
+  'diff',
+  'dockerfile',
+  'elixir',
+  'erlang',
+  'go',
+  'gomod',
+  'gpg',
+  'graphql',
+  'html',
+  'javascript',
+  'jsdoc',
+  'json',
+  'just',
+  'lua',
+  'make',
+  'markdown',
+  'markdown_inline',
+  'python',
+  'query',
+  'rust',
+  'scss',
+  'sql',
+  'terraform',
+  'typescript',
+  'vim',
+  'vimdoc',
+  'yaml',
 }
 
 local function config()
-	require("nvim-treesitter").install(parsers)
+  require('nvim-treesitter').install(parsers)
 
-	vim.api.nvim_create_autocmd("FileType", {
-		pattern = parsers,
-		callback = function()
-			vim.treesitter.start()
-		end,
-	})
+  vim.api.nvim_create_autocmd('FileType', {
+    pattern = parsers,
+    callback = function()
+      vim.treesitter.start()
+    end,
+  })
 end
 
 return {
-	"nvim-treesitter/nvim-treesitter",
-	lazy = false,
-	build = ":TSUpdate",
-	config = config,
+  'nvim-treesitter/nvim-treesitter',
+  lazy = false,
+  build = ':TSUpdate',
+  config = config,
 }
