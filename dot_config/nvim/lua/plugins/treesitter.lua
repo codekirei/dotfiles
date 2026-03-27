4346b31e9495d6ea025f9baaad08d1bbc550df9f
Jacob Blakely <jacob@codekirei.com>
2026-03-27 15:58:29 -0700

    nvim: modernize treesitter

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,49 +1,50 @@
-local opts = {
-  ensure_installed = {
-    'c',
-    'css',
-    'csv',
-    'diff',
-    'dockerfile',
-    'elixir',
-    'erlang',
-    'go',
-    'gomod',
-    'gpg',
-    'graphql',
-    'html',
-    'javascript',
-    'jsdoc',
-    'json',
-    'lua',
-    'make',
-    'markdown',
-    'markdown_inline',
-    'norg',
-    'python',
-    'query',
-    'rust',
-    'scss',
-    'sql',
-    'terraform',
-    'typescript',
-    'vim',
-    'vimdoc',
-    'yaml',
-  },
-  highlight = { enable = true },
+local parsers = {
+  'c',
+  'css',
+  'csv',
+  'diff',
+  'dockerfile',
+  'elixir',
+  'erlang',
+  'go',
+  'gomod',
+  'gpg',
+  'graphql',
+  'html',
+  'javascript',
+  'jsdoc',
+  'json',
+  'just',
+  'lua',
+  'make',
+  'markdown',
+  'markdown_inline',
+  'python',
+  'query',
+  'rust',
+  'scss',
+  'sql',
+  'terraform',
+  'typescript',
+  'vim',
+  'vimdoc',
+  'yaml',
 }
 
 local function config()
-  require('nvim-treesitter.configs').setup(opts)
-  require('nvim-treesitter.install').compilers = { 'gcc-11' }
-  require('tree-sitter-just').setup({})
+  require('nvim-treesitter').install(parsers)
+
+  vim.api.nvim_create_autocmd('FileType', {
+    pattern = parsers,
+    callback = function()
+      vim.treesitter.start()
+    end,
+  })
 end
 
 return {
   'nvim-treesitter/nvim-treesitter',
+  lazy = false,
+  build = ':TSUpdate',
   config = config,
-  dependencies = {
-    'IndianBoy42/tree-sitter-just',
-  },
 }
