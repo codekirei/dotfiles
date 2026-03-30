52495a652000ea173ae991f72d5a717116df116e
Jacob Blakely <jacob@codekirei.com>
2026-03-30 14:42:55 -0700

    nvim: fix commentstring err

--- dot_config/nvim/lua/plugins/comment.lua
+++ dot_config/nvim/lua/plugins/comment.lua
@@ -1,6 +1,9 @@
 return {
   'numToStr/Comment.nvim',
-  opts = {},
+  opts = {
+    pre_hook = function(ctx)
+      return require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook()(ctx)
+    end,
+  },
   lazy = false,
 }
-
